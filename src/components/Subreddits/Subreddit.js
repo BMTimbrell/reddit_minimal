83cdbd9fd2dd9ee4c0806f1ce9aa89f7d649a545
Ben <bmtimbrell@gmail.com>
2023-09-30 21:39:29 +0100

    selecting a subreddit will display posts from subreddit

--- src/components/Subreddits/Subreddit.js
+++ src/components/Subreddits/Subreddit.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import './Subreddit.css';
+import { setSelectedSubreddit } from '../RedditPosts/redditPostsSlice';
+import { useDispatch } from 'react-redux';
+import { useParams, useNavigate } from 'react-router-dom';
 
 function Subreddit({ name, icon }) {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    let { postId } = useParams();
+    const handleOnClick = () => {
+        if (postId) navigate('/posts');
+        dispatch(setSelectedSubreddit(name.substring(2)));
+    };
+
     return (
-        <div className="subreddit">
+        <div className="subreddit" onClick={handleOnClick}>
             {
                 !icon && <img
                     src="https://styles.redditmedia.com/t5_5s5qbl/styles/communityIcon_hkq7zlki8ug81.png"
