d05da614dd387dd7e911ebd6855b376d53ca7969
Ben <bmtimbrell@gmail.com>
2023-09-30 15:10:54 +0100

    improved header style

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './Header.css';
+import { useSelector } from 'react-redux';
+import { selectSubreddit } from '../RedditPosts/redditPostsSlice';
 
 function Header() {
+    const subreddit = useSelector(selectSubreddit);
+    const onChangeHandler = () => {
+
+    };
+
     return (
         <header>
             <h1>Reddit Minimal</h1>
-            <input type="text" placeholder={`Search posts`} />
+            <input
+                type="text" 
+                placeholder={`Search posts in r/${subreddit}`} 
+                onChange={onChangeHandler} 
+            />
         </header>
     );
 }
