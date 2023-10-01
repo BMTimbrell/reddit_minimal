504968a4d9c0e2b3577e19182ae9b93a5bf2209f
Ben <bmtimbrell@gmail.com>
2023-10-01 17:58:57 +0100

    scrolls to top of page when pathname changes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from '../Header/Header';
-import { Outlet, Navigate, useParams } from 'react-router-dom';
+import { Outlet, Navigate, useParams, useLocation } from 'react-router-dom';
 import './Home.css';
 import Subreddits from '../Subreddits/Subreddits';
 
 function Home() {
     const { postId } = useParams();
+    const  { pathname } = useLocation();
+    
+    // Scrolls to top of page whenever path changes
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [pathname]);
+
     return(
         <div className="home">
             <Header className="header" />
