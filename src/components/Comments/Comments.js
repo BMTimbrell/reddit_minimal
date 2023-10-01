f29c82f8a852586f854faa8b72d5e4f8fc1335cf
Ben <bmtimbrell@gmail.com>
2023-10-01 17:51:16 +0100

    made list items look centered better in comments

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Comment from './Comment';
 import { selectLoadingComments, selectErrorComments } from '../RedditPosts/redditPostsSlice';
 import { useSelector } from 'react-redux';
+import './Comments.css';
 
 function Comments({ comments }) {
     const messageStyle = {fontWeight: 'bold', margin: '0.625rem', fontSize: '20px'};
@@ -11,13 +12,13 @@ function Comments({ comments }) {
     if (loadingComments) return <p style={messageStyle}>Loading comments...</p>;
     if (comments.length === 0) return <p style={messageStyle}>No replies found</p>;
     return (
-        <>
+        <div className="comments">
             {   
                 comments.map(comment => (
                     <Comment key={comment.data.id} comment={comment.data} />
                 ))
             }
-        </>
+        </div>
     );
 }
 
